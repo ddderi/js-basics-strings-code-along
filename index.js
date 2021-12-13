8e65f30e3bf4c04d114657bacf27841b5f73ebf8
ddderi <maxime830@live.fr>
2021-12-13 01:14:44 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+let currentUser = "string";
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+
+// let welcomeMessage = 'Welcome to Flatbook, ' + ${currentUser} + '!';
+
+// function welcomeMessage () {
+//     console.log('Welcome to Flatbook, ' + currentUser + '!');
+// }
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+
+// let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + ${currentUser} + '!';
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser} !`;
+
+// let shortGreeting = 'Welcole
+
+let shortGreeting = `Welcome, ${firstInitial} !`;
+
